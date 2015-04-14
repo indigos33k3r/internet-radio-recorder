125164e9431dd23bfa122ac0da27eb1d3bf9653f
Marcus Rohrmoser <github@mro.name>
2015-04-14 22:22:01 +0200

    allow an optional subject (e.g. broadcast brand) (fixes)

--- htdocs/app/scraper-br.rb
+++ htdocs/app/scraper-br.rb
@@ -199,8 +199,8 @@ module Recorder
         end
       end
 
-      subj_node = doc.at_css('a.link_broadcast.link_indexPage')
-      bc.DC_subject = bc.src_url + subj_node[:href].to_s.gsub(/\/index\.html/,'/') unless subj_node.nil?
+      subj_node = doc.at_css('a.link_broadcast.link_broadcast_tv')
+      bc.DC_subject = bc.src_url + subj_node[:href].to_s unless subj_node.nil? || subj_node[:href].nil?
 
       doc = doc.at_css '.detail_inlay'
   
