5e0a720b99e68a27a91ffc88f94130d389221fa2
Marcus Rohrmoser <github@mro.name>
2015-12-17 20:09:39 +0100

    go! refs #29

--- htdocs/app/cron/hourly.sh
+++ htdocs/app/cron/hourly.sh
@@ -19,27 +19,33 @@
 #
 # MIT License http://opensource.org/licenses/MIT
 
-parallel --version >/dev/null || { echo "install 'parallel'" && exit 1;}
-
 cd "$(dirname "$0")"/../..
 me="$(basename "$0")"
 
-/bin/date 1>> log/"$me".stdout.log 2>> log/"$me".stderr.log
-for scraper in stations/*/app/scraper.??
-do
-  case "$scraper" in
-    *.rb)
-      echo "bundle exec $scraper --incremental"
-    ;;
-    *)
-      echo "$scraper --incremental"
-    ;;
-  esac
-done \
-| parallel 2>> log/"$me".stderr.log \
+echo "Start  $(/bin/date +'%F %T')" 1>> log/"$me".stdout.log 2>> log/"$me".stderr.log
+
+if [ -x ../bin/scrape-linux-amd64-0.0.1 ] ; then
+  ../bin/scrape-linux-amd64-0.0.1 2>> log/"$me".stderr.log
+else
+  parallel --version >/dev/null || { echo "install 'parallel'" && exit 1;}
+
+  for scraper in stations/*/app/scraper.??
+  do
+    case "$scraper" in
+      *.rb)
+        echo "bundle exec $scraper --incremental"
+      ;;
+      *)
+        echo "$scraper --incremental"
+      ;;
+    esac
+  done \
+  | parallel 2>> log/"$me".stderr.log
+fi \
+| tee log/"$me".stdout.dat \
 | app/broadcast-render.lua --stdin 2>> log/"$me".stderr.log \
 1>> log/"$me".stdout.log
 
 nice app/calendar.lua stations/* podcasts/* 1>> log/"$me".stdout.log 2>> log/"$me".stderr.log
 
-/bin/date 1>> log/"$me".stdout.log
+echo "Finish $(/bin/date +'%F %T')" 1>> log/"$me".stdout.log
